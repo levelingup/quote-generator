841866742c0cab17dea681c46f230d2f3b2649c2
Andrew Liu <ahliu1@gmail.com>
2022-07-12 22:55:00 -0700

    first init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
